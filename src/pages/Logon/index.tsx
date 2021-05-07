e43b5668473a762b69ca41675c25b6b1f67b2d52
Jeferson-Mendes <jefersonmendes3c@gmail.com>
2021-05-06 23:48:43 -0300

    clear project and login screen

---
+++ src/pages/Logon/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './style.css';
+import logo from '../../assets/logo-2.svg';
+
+const Home = () => {
+
+    return (
+        <div id="login-container">
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <h2 className="title">Seja bem vindo!</h2>
+            <div className="form-field">
+                <form >
+                    <label htmlFor="login">Login</label>
+                    <input type="text" name="login"/>
+
+                    <label htmlFor="password">Senha</label>
+                    <input type="text" name="password"/>
+                </form>
+            </div>
+            <span className="forget-pass">Esqueceu a senha?</span>
+            <div className="entry-button">
+                <button>Entrar</button>
+            </div>
+            <div className="signup">
+                <p>Ainda não possui conta?</p>
+                <span>Cadastre-se</span>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
